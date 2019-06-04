78c8c17208984971a9521a08027a4aea8484a43c
Alsidu <alvaro_silva_duran@hotmail.com>
2019-06-04 13:35:59 +0200

    Dibujo ya retocado y acabado

--- juego1.js
+++ juego1.js
@@ -6,62 +6,69 @@ function dibujar(fallos) {
     while (l<=fallos) {
         if (fallos == 0 || 0<= fallos) {
             /* horca */
-            ctx.moveTo(30, 250);
-            ctx.lineTo(30, 50);
-            ctx.lineTo(125, 50);
-            ctx.lineTo(125, 100);
+            ctx.moveTo(50, 550);
+            ctx.lineTo(50, 50);
+            ctx.lineTo(200, 50);
+            ctx.lineTo(200, 100);
             ctx.stroke();
-            ctx.moveTo(30, 90);
-            ctx.lineTo(75, 50);
+            ctx.moveTo(50, 140);
+            ctx.lineTo(130, 50);
+            ctx.lineTo(50, 50);
+            ctx.lineTo(90,95);
             ctx.stroke();
+            ctx.fillRect(0,550,300,300)
         }
 
         if (fallos == 1 || 1<= fallos) {
-            /*cabeza*/
+            /* cabeza */
             ctx.beginPath();
-            ctx.arc(125, 115, 15, 0, 2 * Math.PI);
+            ctx.arc(200, 120, 20, 0, 2 * Math.PI);
             ctx.stroke();
         }
         if (fallos == 2 || 2<= fallos) {
             /* cuerpo */
-            ctx.moveTo(125, 130);
-            ctx.lineTo(125, 190);
+            ctx.moveTo(200, 140);
+            ctx.lineTo(200, 200);
             ctx.stroke();
         }
         if (fallos == 3 || 3<= fallos) {
-            /* brazo izquierdo*/
-            ctx.moveTo(125, 140);
-            ctx.lineTo(110, 160);
+            /* brazo izquierdo */
+            ctx.moveTo(200, 150);
+            ctx.lineTo(170, 170);
             ctx.stroke();
         }
         if (fallos == 4 || 4<= fallos) {
-            /* brazo derecho*/
-            ctx.moveTo(125, 140);
-            ctx.lineTo(140, 160);
+            /* brazo derecho */
+            ctx.moveTo(200, 150);
+            ctx.lineTo(230, 170);
             ctx.stroke();
         }
         if (fallos == 5 || 5<= fallos) {
-            /* pierna izquierda*/
-            ctx.moveTo(125, 190);
-            ctx.lineTo(110, 220);
+            /* pierna izquierda */
+            ctx.moveTo(200, 200);
+            ctx.lineTo(170, 240);
             ctx.stroke();
         }
         if (fallos == 6 || 6<= fallos) {
-            /* pierna derecha*/
-            ctx.moveTo(125, 190);
-            ctx.lineTo(140, 220);
+            /* pierna derecha */
+            ctx.moveTo(200, 200);
+            ctx.lineTo(230, 240);
             ctx.stroke();
-            ctx.moveTo(115,110);
-            ctx.lineTo(125,115);
+            /* Ojos */
+            ctx.moveTo(185,115);
+            ctx.lineTo(195,120);
             ctx.stroke();
-            ctx.moveTo(126,110);
-            ctx.lineTo(136,115);
+            ctx.moveTo(205,115);
+            ctx.lineTo(215,120);
             ctx.stroke();
-            ctx.moveTo(125,110);
-            ctx.lineTo(115,115);
+            ctx.moveTo(195,115);
+            ctx.lineTo(185,120);
             ctx.stroke();
-            ctx.moveTo(136,110);
-            ctx.lineTo(126,115);
+            ctx.moveTo(215,115);
+            ctx.lineTo(205,120);
+            ctx.stroke();
+            ctx.moveTo(195,130);
+            ctx.lineTo(205,130);
             ctx.stroke();
         }
         if (fallos == l){
@@ -72,4 +79,4 @@ function dibujar(fallos) {
     }
 }
 
-dibujar(0);
+dibujar(6);
