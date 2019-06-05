08bb1c77b7360258aa5d0dc7e32b3de73472632e
mda18 <martada2697@gmail.com>
2019-06-05 12:58:59 +0200

    efectos

---
+++ efectos.js
@@ -0,0 +1,18 @@
+
+                
+                var col; 
+           function colores() { 
+               var co = document.getElementById("colors") 
+             co.style.color = co.style.color == "coral" ? "darkviolet" : "coral";
+               
+               
+           }
+                  col = setInterval(colores, 500);
+
+
+
+
+
+
+
+
